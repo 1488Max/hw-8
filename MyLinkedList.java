4c733d536c76a9bdc0957e043354f9af42293d38
1488Max <bargamin123@gmail.com>
2022-01-26 20:11:52 +0200

    а

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -11,10 +11,35 @@ class MyLinkedList<E> {
             this.next = next;
             this.prev = prev;
         }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public E getItem() {
+            return item;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
     }
     private int size = 0;
     private Node<E> first;
     private Node<E> last;
+
+    public Node<E> getFirst() {
+        return first;
+    }
+
+    public Node<E> getLast() {
+        return  last;
+    }
+
     public <E> void add(E object){
         if(this.size == 0){
             Node newObject = new Node(0,object,null,null);
@@ -29,9 +54,25 @@ class MyLinkedList<E> {
 
         }
     }
+
+    public Node getPrevious(Node node){
+        return node.prev;
+    }
+    public E get(int index){
+       Node realLast = (Node) getLast();
+
+       for(int i = 0; i < size()-index-1;i++) {
+           realLast = getPrevious(realLast);
+       }
+
+
+       return (E) realLast.item;
+    }
+
     public int size(){
         return size;
     }
+
     public void clear(){
         this.size = 0;
         this.last = null;
@@ -45,5 +86,8 @@ class LinkedTest{
         lol.add(1);
         lol.add(5);
         lol.add(1);
+        System.out.println("lol.get(2) = " + lol.get(1));
+        System.out.println("lol.getLast() = " + lol.getLast());
+        System.out.println("lol.size() = " + lol.size());
     }
 }
